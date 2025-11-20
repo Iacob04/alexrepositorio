aa0e98748ebcadedbeefd32ab85ced4dc9431d47
Alexandru <alexiacob2004@gmail.com>
2025-11-20 12:33:07 +0100

    eclipse del 20 de noviembre jodeeeerr

---
+++ eclipse-workspace/Clase/src/clase/Clase_18_Noviembre.java
@@ -0,0 +1,22 @@
+
+package clase;
+
+import java.util.Scanner;
+
+public class Clase_18_Noviembre {
+
+	public static void main(String[] args) {
+		// ejercicio 6 boletín 8
+		 Scanner teclado = new Scanner(System.in);
+		 System.out.print("Introduzca el texto para comprobar si es un palindromo ");
+		 
+		 String palindromo = teclado.next();
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+
+}
